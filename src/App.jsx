1a3781d959bd65b59b4a2a76b3372efba6e7a45a
abram <abrambagus@gmail.com>
2025-06-04 14:23:23 +0700

    integrate sentry

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
+import * as Sentry from "@sentry/react";
+
 const App = () => {
   return (
     <main className="bg-black">
@@ -14,4 +16,6 @@ const App = () => {
   );
 };
 
-export default App;
+const ProfiledApp = Sentry.withProfiler(App);
+
+export default ProfiledApp;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,33 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://f2b310253deb9c4869120c5948f5d2b5@o4509433891782656.ingest.us.sentry.io/4509433894600704",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
+
+// Sentry.init({
+//   dsn: "https://f2b310253deb9c4869120c5948f5d2b5@o4509433891782656.ingest.us.sentry.io/4509433894600704",
+//   integrations: [
+//     Sentry.browserTracingIntegration(),
+//     Sentry.reactRouterV6BrowserTracingIntegration({
+//       useEffect: React.useEffect,
+//     }),
+//     Sentry.replayIntegration({
+//       maskAllText: false,
+//       blockAllMedia: false,
+//     }),
+//   ],
+//   tracesSampleRate: 1.0,
+//   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1.0,
+// });
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
